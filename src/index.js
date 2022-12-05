2a1c006f0e4d367039df75791ab986050836cf82
HarshVaishnani29 <harshvaishnani2900@gmail.com>
2022-12-05 11:53:31 +0530

    Bootstrap-Header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Custom from './components/Custom';
+// import Custom from './components/Custom';
+import Header from './components/Header';
 // import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const list = ["Home","Pages","Contact Us","Services",];
+// const list = ["Home","Pages","Contact Us","Services",];
+const menu = ["Projects","About-Us","Address","Gallery",];
 root.render(
   <>
-  {/* <Header logo="images/logo.png"  /> */}
+  <Header logo="images/logo.png"  menu ={menu}/>
   {/* <App/> */}
-  <Custom logo="images/logo.png" list = {lists}/>
+  {/* <Custom logo="images/logo.png" list = {lists}/> */}
   </>
 );
