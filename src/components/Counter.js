426d2c14a2fdffa53a88de99858a0c73bea08ea0
HarshVaishnani29 <harshvaishnani2900@gmail.com>
2022-12-06 08:33:00 +0530

    Counter Commit

---
+++ src/components/Counter.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+import Counters from "../components/Counter.css"
+
+class Counter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+    increment() {
+        this.setState(() => {
+            return {
+            count: this.state.count + 1
+        }
+
+        })
+    }
+    decrement() {
+        if (this.state.count > 0) {
+            this.setState({
+                count: this.state.count - 1
+            })
+        }
+    }
+    delete() {
+        this.setState({
+          count: 0
+        })
+      }
+    
+    render() {
+        return (
+            <>
+                <div className="container">
+                    <div className="row">
+                        <div className="main-nav">
+                            <li>
+                                <a href="#">
+                                    <img src="images/dmart.png" />
+                                </a>
+                            </li>
+
+                            <h1>
+                                D-mart
+                            </h1>
+
+                            <h2>
+                                Products
+                            </h2>
+
+                            <h3>
+                                Kurkure Masala Munch
+                            </h3>
+                            <img src="images/kurkure.png" className="pro2" />
+                            <p>
+                                {this.state.count}
+                            </p>
+                            <div className="addremovebtn">
+                                <button onClick={()=>{this.increment()}}>
+                                <i class="fa-solid fa-plus"></i>
+                                </button>
+                                <button onClick={()=>{this.decrement()}}>
+                                <i class="fa-solid fa-minus"></i>
+                                </button>
+                                <button className="Delete" onClick={()=>{this.delete()}}>
+                                <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import Custom from './components/Custom';
-import Header from './components/Header';
+// import Header from './components/Header';
 // import App from './components/App';
+import Counter from "../src/components/Counter";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const list = ["Home","Pages","Contact Us","Services",];
-const menu = ["Projects","About-Us","Address","Gallery",];
+// const menu = ["Projects","About-Us","Address","Gallery",];
 root.render(
   <>
-  <Header logo="images/logo.png"  menu ={menu}/>
   {/* <App/> */}
+  {/* <Header logo="images/logo.png"  menu ={menu}/> */}
   {/* <Custom logo="images/logo.png" list = {lists}/> */}
+  <Counter/>
   </>
 );
