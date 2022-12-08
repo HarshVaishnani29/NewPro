2ae1fd19201b769ff00b97e0e6ada65291eb8348
HarshVaishnani29 <harshvaishnani2900@gmail.com>
2022-12-08 07:59:06 +0530

    Custom Header

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Custom from './components/Custom';
+// import Custom from './components/Custom';
+// import Header from './components/Header';
 // import App from './components/App';
+// import Counter from "../src/components/Counter";
+import Themepicker from '../src/components/ThemePicker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const list = ["Home","Pages","Contact Us","Services",];
+// const list = ["Home","Pages","Contact Us","Services",];
+// const menu = ["Projects","About-Us","Address","Gallery",];
 root.render(
   <>
-  {/* <Header logo="images/logo.png"  /> */}
   {/* <App/> */}
-  <Custom logo="images/logo.png" list = {lists}/>
+  {/* <Header logo="images/logo.png"  menu ={menu}/> */}
+  {/* <Custom logo="images/logo.png" list = {lists}/> */}
+  {/* <Counter/> */}
+  <Themepicker/>
+  
   </>
 );
